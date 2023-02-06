ee856c33b8fd6da9756099756cb79356c6551757
stanis <stanis@netvolution.fr>
2023-02-06 17:10:37 +0100

    Détail d'une région affichage du nom

--- detail_region.php
+++ detail_region.php
@@ -1,4 +1,11 @@
 <?php
-echo "<h1>Détail d'une région</h1>";
+require_once 'db.php';
 
 $code_region = $_GET['code_region'];
+
+$sql = "SELECT * FROM regions WHERE code = :code_region";
+$stmt = $db->prepare($sql);
+$stmt->execute(['code_region' => $code_region]);
+$region = $stmt->fetchObject('Region');
+
+echo "<h1>" . $region->name . "</h1>";
