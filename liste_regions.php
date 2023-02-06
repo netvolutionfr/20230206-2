a3d7f60d879df9527d8614f6001f0a0c1f1b2663
stanis <stanis@netvolution.fr>
2023-02-06 16:54:42 +0100

    affichage de la liste des régions

--- liste_regions.php
+++ liste_regions.php
@@ -1,1 +1,13 @@
 <?php
+require_once 'db.php';
+
+// Récupération des régions
+$sql = "SELECT * FROM regions ORDER BY name";
+$stmt = $db->query($sql);
+$regions = $stmt->fetchAll(PDO::FETCH_CLASS, 'Region');
+
+echo "<h1>Liste des régions</h1>";
+
+foreach ($regions as $region) {
+    echo $region->name . "<br>";
+}
